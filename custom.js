acb29aab279ef31ae222cfccab414948f383a259
YoonJunHyeon <qmdldos@gmail.com>
2023-11-24 19:57:10 +0900

    -- 중간저장

--- custom.js
+++ custom.js
@@ -2,8 +2,11 @@ console.log('hi');
 
 const nav = document.getElementById('nav');
 const mainNav = document.getElementById('mainNav');
-const 
+const subMenu = document.querySelectorAll('.submenuUl');
+console.log(subMenu);
 
 mainNav.addEventListener('mouseover',()=>{
-  
+  subMenu.forEach((submenuUl)=>{
+    submenuUl.style.height = "160px";
+  })
 });
